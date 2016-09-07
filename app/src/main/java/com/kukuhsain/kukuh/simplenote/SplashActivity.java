3fe2a28362b8bdd8f77314292f8520093be009fb
Kukuh <kukuhsain@gmail.com>
2016-09-07 16:59:09 +0700

    Add login activity and route splash activity to login activity after a few second

--- app/src/main/java/com/kukuhsain/kukuh/simplenote/SplashActivity.java
+++ app/src/main/java/com/kukuhsain/kukuh/simplenote/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.kukuhsain.kukuh.simplenote;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
     }
 }
