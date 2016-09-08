7df635e4007ac5724e9890ed9a067cdea318009b
Kukuh <kukuhsain@gmail.com>
2016-09-08 13:21:47 +0700

    Add facebook login button

--- app/src/main/java/com/kukuhsain/kukuh/simplenote/LoginActivity.java
+++ app/src/main/java/com/kukuhsain/kukuh/simplenote/LoginActivity.java
@@ -3,6 +3,8 @@ package com.kukuhsain.kukuh.simplenote;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.FacebookSdk;
+
 /**
  * A login screen that offers login via email/password.
  */
@@ -11,6 +13,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
     }
 }
